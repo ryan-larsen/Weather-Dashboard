427c16e856dba314aa067da195264b8b0f571005
Ryan Larsen <larsenr12@gmail.com>
2019-12-16 13:31:41 -0500

    Added ajax and event listener to button

---
+++ script.js
@@ -0,0 +1,51 @@
+var cityInput = $("#userInputBtn").val().trim();
+
+$("#saveBtn").on("click", function(event) {
+    // Preventing the buttons default behavior when clicked (which is submitting a form)
+    event.preventDefault();
+    // This line grabs the input from the textbox
+    
+    var inputBtn = ('<button class="newBtn">')
+
+    // Adding the movie from the textbox to our array
+    $('#btnContainer').append(inputBtn).append(cityInput)
+
+  });
+
+  var APIKey = "d3c45428b267f056e0b15460332c1bf1";
+
+  // Here we are building the URL we need to query the database
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
+    "q=" + cityInput + "&units=imperial&appid=" + APIKey;
+
+  // Here we run our AJAX call to the OpenWeatherMap API
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    // We store all of the retrieved data inside of an object called "response"
+    .then(function(response) {
+
+      // Log the queryURL
+      console.log(queryURL);
+
+      // Log the resulting object
+      console.log(response);
+
+      // Transfer content to HTML
+      $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+      $(".wind").text("Wind Speed: " + response.wind.speed);
+      $(".humidity").text("Humidity: " + response.main.humidity);
+      $(".temp").text("Temperature (F) " + response.main.temp);
+
+      // Converts the temp to Kelvin with the below formula
+      var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+      $(".tempF").text("Temperature (Kelvin) " + tempF);
+
+      // Log the data in the console as well
+      console.log("Wind Speed: " + response.wind.speed);
+      console.log("Humidity: " + response.main.humidity);
+      console.log("Temperature (F): " + response.main.temp);
+    });
+
+
